72ef8a9557afc48a4897ab2083dfd19ff2179599
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-08 20:08:43 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,12 @@
 class Hash
   def keys_of(*arguments)
     keys = []
-    Hash.each do |
+    Hash.each do |key, value|
+      arguments.each do |argument|
+        if argument == value
+          keys.push(key)
+        end
+      end
+    end
   end
 end
